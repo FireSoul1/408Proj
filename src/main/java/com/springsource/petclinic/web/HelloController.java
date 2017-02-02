b3693a45b2faabec431d605d1151e048eddeba14
FireSoul1 <dragonball1>
2017-02-02 00:25:33 -0500

    Temp routes

---
+++ src/main/java/com/springsource/petclinic/web/HelloController.java
@@ -0,0 +1,23 @@
+package com.springsource.petclinic.web;
+
+
+
+
+@Controller
+public class HelloController {
+   private String message;
+
+   public void setMessage(String message){
+      this.message  = message;
+   }
+
+
+   @RequestMapping("/")
+   public ModelAndView starting() {
+        String message = "WELCOME SPRING MVC";
+        return new ModelAndView("welcomepage", "message", message);
+   }
+   public void getMessage(){
+      System.out.println("Your Message : " + message);
+   }
+}
