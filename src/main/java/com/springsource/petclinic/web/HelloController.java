95241aefe47d9231c292c9296c30b634680d941c
FireSoul1 <dragonball1>
2017-02-02 12:42:18 -0500

    WHat even

--- src/main/java/com/springsource/petclinic/web/HelloController.java
+++ src/main/java/com/springsource/petclinic/web/HelloController.java
@@ -9,16 +9,10 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloController {
    private String message;
-
-   // public void setMessage(String message){
-   //    this.message  = message;
-   // }
    @RequestMapping("/")
    public ModelAndView starting() {
         String message = "WELCOME SPRING MVC";
         return new ModelAndView("welcomepage", "message", message);
    }
-   // public void getMessage(){
-   //    System.out.println("Your Message : " + message);
-   // }
+
 }
