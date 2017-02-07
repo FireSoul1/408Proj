2d1da3c69f2c7840c8ad28307ae9589208316648
Matt Pace <pace4@purdue.edu>
2017-02-07 10:52:26 -0500

    Update to webpack 2, set up theme loading

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,22 +1,45 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack')
+const path = require('path')
 
 var BUILD_DIR = path.resolve(__dirname, 'src/main/resources/static');
-var APP_DIR = path.resolve(__dirname, 'client');
+const APP_DIR = path.resolve(__dirname, 'client')
 
-var config = {
+const config = {
   devtool: 'hidden-source-map',
   entry: APP_DIR + '/components/App.jsx',
   output: {
     path: BUILD_DIR,
     filename: 'js/bundle.js'
   },
-  module : {
-    loaders : [
+  resolve: {
+    modules: [
+      APP_DIR,
+      'node_modules'
+    ],
+    extensions: ['.js', '.jsx', '.less']
+  },
+  module: {
+    rules: [
       {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel-loader'
+        test: /\.jsx?/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf)$/,
+        loader: 'url-loader?limit=10000&name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.(png|jpg|svg)$/,
+        loader: 'url-loader?limit=10000&name=img/[name].[ext]'
       }
     ]
   },
@@ -25,8 +48,28 @@ var config = {
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
       }
-    })
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+        drop_debugger: true,
+        drop_console: true,
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
   ]
-};
+}
 
-module.exports = config;
+module.exports = config
