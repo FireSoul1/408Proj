53679efad4079d6aaef27e42c270abdb2226f5a7
Matt Pace <pace4@purdue.edu>
2017-02-07 10:52:26 -0500

    Update to webpack 2, set up theme loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,48 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack')
+const path = require('path')
 
 var BUILD_DIR = path.resolve(__dirname, 'src/main/resources/static');
-var APP_DIR = path.resolve(__dirname, 'client');
+const APP_DIR = path.resolve(__dirname, 'client')
 
-var config = {
+const config = {
+  devtool: 'eval',
   entry: APP_DIR + '/components/App.jsx',
   output: {
     path: BUILD_DIR,
     filename: 'js/bundle.js'
   },
-  module : {
-    loaders : [
+  resolve: {
+    modules: [
+      APP_DIR,
+      'node_modules'
+    ],
+    extensions: ['.js', '.jsx', '.less']
+  },
+  module: {
+    rules: [
       {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel-loader'
+        test: /\.jsx?/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf)$/,
+        loader: 'url-loader?limit=10000&name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.(png|jpg|svg)$/,
+        loader: 'url-loader?limit=10000&name=img/[name].[ext]'
       }
     ]
   }
-};
+}
 
-module.exports = config;
+module.exports = config
