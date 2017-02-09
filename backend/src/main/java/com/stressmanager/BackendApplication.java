77874f058bbb1211db513fb3cc3a2729235e2e11
Matt Pace <pace4@purdue.edu>
2017-02-08 20:31:16 -0500

    Add Tomcat webserver to serve static content

--- backend/src/main/java/com/stressmanager/BackendApplication.java
+++ backend/src/main/java/com/stressmanager/BackendApplication.java
@@ -2,9 +2,16 @@ package com.stressmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BackendApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
