94a6c02ca805f20c5dc34d594ff3418e8a29918f
FireSoul1 <dragonball1>
2017-02-10 15:00:15 -0500

    Intial setup

--- src/main/java/com/stressmanager/BackendApplication.java
+++ src/main/java/com/stressmanager/BackendApplication.java
@@ -6,7 +6,28 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+<<<<<<< Updated upstream
 public class BackendApplication extends SpringBootServletInitializer {
+=======
+@RestController
+@EnableOAuth2Client
+@EnableAuthorizationServer
+@Order(6)
+public class BackendApplication extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	OAuth2ClientContext oauth2ClientContext;
+
+	@RequestMapping({ "/user", "/me" })
+	public Map<String, String> user(Principal principal) {
+		Map<String, String> map = new LinkedHashMap<>();
+		map.put("name", principal.getName());
+
+		System.out.println("authenticated!!!!");
+
+		return map;
+	}
+>>>>>>> Stashed changes
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
--- src/test/java/com/stressmanager/BackendApplicationTests.java
+++ src/test/java/com/stressmanager/BackendApplicationTests.java
@@ -1,16 +1,49 @@
 package com.stressmanager;
 
-import org.junit.Test;
+//JUnit
 import org.junit.runner.RunWith;
+//Spring
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+//TestNG
+import org.testng.Assert;
+import org.junit.Test;
+
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BackendApplicationTests {
+public class BackendApplicationTests extends AbstractTestNGSpringContextTests {
+    @Autowired
+    private BackendApplication controller;
+    @Autowired
+    private TestRestTemplate restTemplate;
 
-	@Test
-	public void contextLoads() {
+    @Test
+	public void homeNotNull() throws Exception{
+        System.out.println("\nRunnning test case 1: Checking that maincontroller is working.");
+        Assert.assertNotNull(controller);
 	}
+    @Test
+	public void checkHomePage() throws Exception{
+        System.out.println("\nRunnning test case 2: Checking that HomePage loads up.");
+        
+
+	}
+    @Test
+	public void DBSetUpLocal() throws Exception{
+        System.out.println("\nRunnning test case 2: Checking that the Local DB is setup.");
+
+	}
+
 
 }
