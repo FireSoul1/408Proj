e9189b143b56bc0c7f15e2df15a38387b6507e18
Joseph Khawly <josephkhawly@gmail.com>
2017-02-16 15:41:26 -0500

    Added Google Calendar API to dependencies.

--- src/main/java/com/stressmanager/MainController.java
+++ src/main/java/com/stressmanager/MainController.java
@@ -5,6 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.api.client.auth.oauth2.Credential;
+// import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+// import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.util.DateTime;
+
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.*;
+
 @Controller
 @Component
 public class MainController {
@@ -18,5 +30,11 @@ public class MainController {
 
     //POST that takes calID and request calendar from API and parse it.
     // Send "OK" as a response.
+    @RequestMapping(value = "/calendar") 
+    @ResponseBody
+    public String calendar(String calendarId) {
+
+        return "OK";
+    }
 
 }
