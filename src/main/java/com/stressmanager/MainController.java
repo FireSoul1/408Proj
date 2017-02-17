2b1a9eda0dba17d04cf9dadd74cb5e6ba4d6913f
FireSoul1 <dragonball1>
2017-02-17 10:53:41 -0500

    Change Maven Build. Slower, but work(?)

--- src/main/java/com/stressmanager/MainController.java
+++ src/main/java/com/stressmanager/MainController.java
@@ -26,52 +26,56 @@ import java.util.List;
 public class MainController {
 
     // SimpleLog logger = new SimpleLog("MainController");
-    @Autowired 
-    static OAuth2ClientContext oauth2ClientContext;
+    //@Autowired
+    //static OAuth2ClientContext oauth2ClientContext;
 
     //A route to just test out the Spring Framework
     @RequestMapping(value = "/ping")
-    // @ResponseBody
+    @ResponseBody
     public String ping() {
         return "Pong";
     }
     //A route to just test out the Spring Framework
     @RequestMapping(value = "/calendar/list")
     @ResponseBody
-    
-    public static Credential authorize() throws Exception {
-		final List<String> SCOPES = Arrays.asList(CalendarScopes.CALENDAR);
-
-		TokenResponse tolkien = new TokenResponse();
-		tolkien.setAccessToken(oauth2ClientContext.getAccessToken().toString());
-
-		Credential credz = new Credential(BearerToken.authorizationHeaderAccessMethod())
-			.setFromTokenResponse(tolkien);
-		System.out.println("authorized!!!");
-		return credz;
-
-	}
-
-	public static com.google.api.services.calendar.Calendar getCalendarService() throws Exception {
-		final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-		HttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-		Credential credz = authorize();
-
-		return new com.google.api.services.calendar.Calendar.Builder(
-			HTTP_TRANSPORT, JSON_FACTORY, credz)
-			.setApplicationName("Stressmanager")
-			.build();
-	}
-
-	public String pingTemp() throws Exception{
+    public String pingTemp() throws Exception{
         CalendarList callist = BackendApplication.service.calendarList().list().execute();
 
-		List<CalendarListEntry> list = callist.getItems();
-		for (CalendarListEntry event : list) {
-		     System.out.printf("%s (%s)\n", event.getSummary(), event.getColorId());
-		}
+        List<CalendarListEntry> list = callist.getItems();
+        for (CalendarListEntry event : list) {
+             System.out.printf("%s (%s)\n", event.getSummary(), event.getColorId());
+        }
 
         return callist.toPrettyString();
     }
+    // public static Credential authorize() throws Exception {
+	// 	final List<String> SCOPES = Arrays.asList(CalendarScopes.CALENDAR);
+    //
+	// 	TokenResponse tolkien = new TokenResponse();
+    //     if(oauth2ClientContext == null) {
+    //         System.out.println("");
+    //         return null;
+    //     }
+	//     tolkien.setAccessToken(oauth2ClientContext.getAccessToken().toString());
+    //
+	// 	Credential credz = new Credential(BearerToken.authorizationHeaderAccessMethod())
+	// 		.setFromTokenResponse(tolkien);
+	// 	System.out.println("authorized!!!");
+	// 	return credz;
+    //
+	// }
+
+	// public static com.google.api.services.calendar.Calendar getCalendarService() throws Exception {
+	// 	final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+	// 	HttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+	// 	Credential credz = authorize();
+    //
+	// 	return new com.google.api.services.calendar.Calendar.Builder(
+	// 		HTTP_TRANSPORT, JSON_FACTORY, credz)
+	// 		.setApplicationName("Stressmanager")
+	// 		.build();
+	// }
+
+
 
 }
