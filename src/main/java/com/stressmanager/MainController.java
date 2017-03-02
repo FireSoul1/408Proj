e464c602af38a53c9e20155890a27488add2b9e4
FireSoul1 <dragonball1>
2017-03-02 15:29:05 -0500

    Merge in master

--- src/main/java/com/stressmanager/MainController.java
+++ src/main/java/com/stressmanager/MainController.java
@@ -1,6 +1,9 @@
+
 package com.stressmanager;
 
 import java.util.*;
+
+import javax.servlet.http.*;
 
 import com.google.api.client.http.HttpTransport;
 import com.google.api.services.calendar.CalendarScopes;
@@ -16,15 +19,12 @@ import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
-import java.util.List;
 
 @RestController
 @EnableOAuth2Client
@@ -40,19 +40,36 @@ public class MainController {
     public String ping() {
         return "Pong";
     }
-    //A route to just test out the Spring Framework
-    @RequestMapping(value = "/calendar/list")
-    public ResponseEntity<String> pingTemp() throws Exception{
-        final HttpHeaders httpHeaders = new HttpHeaders();
-        CalendarList callist = BackendApplication.service.calendarList().list().execute();
+    //A route to get the calendarList
+     @RequestMapping(value = "/calendar/list")
+     @ResponseBody
+     public ResponseEntity<String> calList() throws Exception{
+         final HttpHeaders httpHeaders = new HttpHeaders();
+         CalendarList callist = BackendApplication.service.calendarList().list().execute();
+         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+         List<CalendarListEntry> list = callist.getItems();
+         for (CalendarListEntry event : list) {
+              System.out.printf("%s (%s)\n", event.getSummary(), event.getColorId());
+         }
 
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+         return new ResponseEntity<String>(callist.toPrettyString(), httpHeaders, HttpStatus.OK);
+     }
+    //TODO: A route for posting Event with it's value
 
-        List<CalendarListEntry> list = callist.getItems();
-        for (CalendarListEntry event : list) {
-             System.out.printf("%s (%s)\n", event.getSummary(), event.getColorId());
-        }
 
-        return new ResponseEntity<String>(callist.toPrettyString(), httpHeaders, HttpStatus.OK);
+    //A route for getting an event by eventID
+    @RequestMapping(value = "/calendar/event")
+    @ResponseBody
+    public String calendarEvent(HttpServletRequest request) throws Exception{
+
+        //get the eventID
+
+        //get the event from the API
+
+        //return the JSON with the information
+        return "Cool beans";
     }
+
+
+
 }
