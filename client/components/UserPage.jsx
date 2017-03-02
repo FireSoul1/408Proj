a35fd33afb9a2515471c4a335b529d924fc01643
Matt Pace <pace4@purdue.edu>
2017-03-02 15:36:16 -0500

    Add beginning calendar

--- client/components/UserPage.jsx
+++ client/components/UserPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import StressCalendar from './StressCalendar'
+
 class UserPage extends React.Component {
   render() {
     return (
       <div className='container'>
-        <p>Calendar placeholder</p>
+        <StressCalendar />
       </div>
     )
   }
