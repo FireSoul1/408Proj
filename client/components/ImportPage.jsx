266293f92b87e18f162226c2883bf236800e7344
Matt Pace <pace4@purdue.edu>
2017-03-03 12:50:07 -0500

    Maintain state in import page

--- client/components/ImportPage.jsx
+++ client/components/ImportPage.jsx
@@ -7,11 +7,19 @@ import {
 import { map } from 'lodash'
 
 class ImportPage extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      calendarId: ''
+    }
+  }
+
   renderOptions() {
     const { calendarList } = this.props
 
     const options = map(calendarList, calendar => {
-      return (<option value={calendar.id}>{calendar.summary}</option>)
+      return (<option key={calendar.id} value={calendar.id}>{calendar.summary}</option>)
     })
 
     return options
@@ -24,7 +32,11 @@ class ImportPage extends React.Component {
           <p>Please select a calendar to import from the dropdown below</p>
 
           <FormGroup controlId="formControlsSelect">
-            <FormControl componentClass="select" placeholder="select">
+            <FormControl
+              componentClass="select"
+              onChange={e => this.setState({ calendarId: e.target.value })}
+              placeholder="select"
+            >
               {this.renderOptions()}
             </FormControl>
           </FormGroup>
