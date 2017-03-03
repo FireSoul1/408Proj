a898ac3f3f01da7f262e96415a17b13e00f66eab
Matt Pace <pace4@purdue.edu>
2017-03-02 19:04:21 -0500

    Add mock data and calendar

--- client/components/UserPage.jsx
+++ client/components/UserPage.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
+import BigCalendar from 'react-big-calendar'
+import { has } from 'lodash'
+import moment from 'moment'
 
-import StressCalendar from './StressCalendar'
+BigCalendar.momentLocalizer(moment)
 
 class UserPage extends React.Component {
+  accessor(time, event) {
+    const dateTimeString = `${time}.dateTime`
+    const dateString = `${time}.date`
+
+    if (has(event, dateTimeString)) {
+      return moment(event[time]['dateTime']).toDate()
+    } else if (has(event, dateString)) {
+      return moment(event[time]['date']).toDate()
+    }
+  }
+
   render() {
     return (
       <div className='container'>
-        <StressCalendar />
+        <BigCalendar
+          events={this.props.eventList}
+          startAccessor={event => this.accessor('start', event)}
+          endAccessor={event => this.accessor('end', event)}
+          allDayAccessor={event => has(event, 'start.date') && has(event, 'end.date')}
+          titleAccessor='summary'
+        />
       </div>
     )
   }
