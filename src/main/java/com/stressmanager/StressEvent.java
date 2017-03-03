af9ea6f2d9f0e0b839990479707191f574294d6f
FireSoul1 <dragonball1>
2017-03-02 23:01:03 -0500

    First POST Method down

--- src/main/java/com/stressmanager/StressEvent.java
+++ src/main/java/com/stressmanager/StressEvent.java
@@ -8,10 +8,14 @@ public class StressEvent {
     //stress level
     int stresslvl;
 
+    //data that Google gives each event
+    String deets;
+
     //constructors
-    StressEvent(int calID, int stress) {
+    StressEvent(int calID, int stress, String deets) {
         calId = calID;
         stresslvl = stress;
+        this.deets = deets;
     }
     //getters
     public int getCalId() {
@@ -20,11 +24,17 @@ public class StressEvent {
     public int getStressLvl() {
         return this.stresslvl;
     }
+    public String getDeets() {
+        return this.deets;
+    }
     //Setters
     public void setCalId(int id) {
         this.calId = id;
     }
     public void setStressLvl(int lvl) {
         this.stresslvl = lvl;
+    }
+    public void setDeets(String deet) {
+        this.deets = deet;
     }
 }
