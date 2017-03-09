46887950b35eb6c2d1f6afb99c8c5a47fec4a412
Matt Pace <pace4@purdue.edu>
2017-03-09 10:07:30 -0500

    Go back to user page after last stress event submitted

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -140,7 +140,7 @@ class App extends React.Component {
     })
   }
 
-  postCalendarEvent(calEvent, stressValue) {
+  postCalendarEvent(calEvent, stressValue, navigateTo) {
     const data = {
       calEvent,
       stressValue,
@@ -153,8 +153,11 @@ class App extends React.Component {
       contentType: 'application/json',
       data: JSON.stringify(data),
       success: () => {
-        // TODO give feedback to user
         console.log(`Added stressValue ${stressValue} to event with id ${calEvent}`)
+
+        if (navigateTo) {
+          this.setActiveView(navigateTo)
+        }
       },
       error: response => {
         // TODO give feedback to user
@@ -189,7 +192,7 @@ class App extends React.Component {
         getCalendars={() => this.getCalendars()}
         getLogout={() => this.getLogout()}
         postCalendarAdd={calId => this.postCalendarAdd(calId)}
-        postCalendarEvent={(calEvent, stressValue) => this.postCalendarEvent(calEvent, stressValue)}
+        postCalendarEvent={(calEvent, stressValue, navigateTo) => this.postCalendarEvent(calEvent, stressValue, navigateTo)}
         unratedEvents={this.unratedEvents()}
         user={this.state.user}
         setActiveView={activeView => this.setActiveView(activeView)}
