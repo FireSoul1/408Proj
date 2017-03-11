5d0a65c2f78a030ac89dde740aea9942f6ca74df
FireSoul1 <dragonball1>
2017-03-11 16:25:21 -0500

    Make sure and use the base package org.openqa.* when importing selenium

--- src/test/java/com/stressmanager/StressFormPageTest.java
+++ src/test/java/com/stressmanager/StressFormPageTest.java
@@ -1,26 +1,55 @@
-import org.seleniumhq.selenium.*;
-import org.seleniumhq.selenium.WebDriver;
-import org.seleniumhq.selenium.remote.DesiredCapabilities;
-import org.seleniumhq.selenium.remote.RemoteWebDriver;
+package com.stressmanager;
+//Selenium
+import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//JUnit and Spring
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.test.context.*;
+import org.junit.runner.RunWith;
+import org.junit.Test;
+//Java
+import java.net.*;
 
-public class StressFormPageTest {
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+@ContextConfiguration
+public class StressFormPageTest{
+
+	@LocalServerPort
+	private int port;
+
 	@Test
-	public static void main(String[] args) throws Exception {
- 
-    DesiredCapabilities caps = DesiredCapabilities.chrome();
-    caps.setCapability("platform", "Mac");
-    caps.setCapability("version", "43.0");
- 
-    WebDriver driver = new RemoteWebDriver("http://localhost:9515", DesiredCapabilities.chrome());
- 	  System.out.println("\nmeowmeowmeow\n");
- 	  System.out.println("arf");
-    /**
-     * Goes to Sauce Lab's guinea-pig page and prints title
-     */
- 
-    driver.get("https://saucelabs.com/test/guinea-pig");
-    System.out.println("title of page is: " + driver.getTitle());
- 
-    driver.quit();
-  }
+	public void testThis() throws MalformedURLException{
+		DesiredCapabilities caps = DesiredCapabilities.chrome();
+	    caps.setCapability("platform", "Mac");
+	    caps.setCapability("version", "43.0");
+
+		WebDriver driver = new RemoteWebDriver("http://localhost:9515", DesiredCapabilities.chrome());
+		
+		try{
+			Thread.sleep(50);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	 	  System.out.println(Colors.ANSI_YELLOW+"\nmeowmeowmeow\n");
+	 	  System.out.println(Colors.ANSI_RED+"arf"+Colors.ANSI_WHITE);
+	    /**
+	     * Goes to Sauce Lab's guinea-pig page and prints title
+	     */
+
+	    driver.get("https://saucelabs.com/test/guinea-pig");
+	    System.out.println("title of page is: " + driver.getTitle());
+
+	    driver.quit();
+
+	}
+
 }
