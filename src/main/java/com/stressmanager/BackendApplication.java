db8018e3a178a24bc9da139f7393911188266890
FireSoul1 <dragonball1>
2017-03-11 23:03:19 -0500

    Polished up a few things, stale code and what not

--- src/main/java/com/stressmanager/BackendApplication.java
+++ src/main/java/com/stressmanager/BackendApplication.java
@@ -58,9 +58,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.MediaType;
 import org.springframework.http.HttpStatus;
 
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;		
-import com.amazonaws.services.dynamodbv2.document.Item;		
-import com.amazonaws.services.dynamodbv2.document.Table;		
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
 
 import com.amazonaws.services.dynamodbv2.document.*;
@@ -292,44 +292,23 @@ public class BackendApplication extends WebSecurityConfigurerAdapter {
 		}
 	}
 
-	// @RequestMapping(value = "/me/find")
-	// public ResponseEntity<String> findUser(Principal principal) throws Exception {
-	// 	String username = principal.getName();
-	// 	String token = oauth2ClientContext.getAccessToken().toString();
-	// 	db.accessDB();
-
-	// 	Table table = db.setup.getUsersTable();
-
-	// 	try { // If user exists, send calendar data.
-	// 		GetItemSpec spec = new GetItemSpec()
-	// 			.withPrimaryKey("userID", username);
-	// 		table.getItem(spec);
-
-	// 		//return events();
-	// 	} catch (Exception e) { // If not, add them to the DB
-	// 		table.putItem(new Item()
-	// 			.withPrimaryKey("userID", username)
-	// 		);
-
-	// 		return new ResponseEntity<String>("New user created", HttpStatus.OK);
-	// 	}
-	// }
-
 	// Display the information for a single event in the user's calendar
 	@RequestMapping(value = "/event/detail")
-	public ResponseEntity<String> eventDetails(String calID, String eventID) throws Exception {
+	@ResponseBody
+	public ResponseEntity<String> eventDetails(@RequestBody GenericJson request) throws Exception {
 		db.accessDB();
 		final HttpHeaders httpHeaders = new HttpHeaders();
 		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
 
 		service = getCalendarService();
+		String calID = (String)request.get("calID");
+		String eventID = (String)request.get("eventID");
 
 		Event event = service.events().get(calID, eventID).execute();
-
-		return new ResponseEntity<String>(event.toPrettyString(), HttpStatus.OK);
+		return new ResponseEntity<String>(event.toPrettyString(), httpHeaders, HttpStatus.OK);
 
 	}
-	
+
 	/*
 	* Spring Security Set up
 	*/
