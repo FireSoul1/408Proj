bf6285a37d3109dfe850b6c361c9798173e22141
Winnie <winnielin@Winnies-MacBook-Pro.local>
2018-02-16 18:00:13 -0500

    add imports

--- client/components/CalendarPage.jsx
+++ client/components/CalendarPage.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
+import {
+  Button,
+  FormControl,
+  FormGroup,
+  Jumbotron
+} from 'react-bootstrap'
+import { map } from 'lodash'
+import SweetAlert from 'react-bootstrap-sweetalert'
 class CalendarPage extends React.Component {
 	
+  constructor(props) {
+  super(props)
+   this.state = {
+      calendarType: 'Google'
+    }
+  }
+
+  
 
 	 render() {
 	 	return (
       <div className='container'>
         <Jumbotron>
           <p>Import Google CalendarPage.</p>
- <Button bsStyle='primary' className='Outlookbtn' >Google </Button>
-        <Button bsStyle='primary' className='Outlookbtn'  >Outlook </Button>
+ <Button bsStyle='primary' className='Outlookbtn' onClick={() => this.setState({calendarType: 'Google'})}>
+        {this.state.calendarType} >Outlook </Button>
+        <Button bsStyle='primary' className='Outlookbtn' onClick={() => this.setState({calendarType: 'Outlook'})}>
+        {this.state.calendarType} >Outlook </Button>
         </Jumbotron>
       </div>
   )
