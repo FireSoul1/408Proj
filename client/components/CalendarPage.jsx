25dccc647892b475cac5d7574d12faac1ed55147
Winnie <winnielin@pal-nat186-60-29.itap.purdue.edu>
2018-02-17 19:50:38 -0500

    google on click button

--- client/components/CalendarPage.jsx
+++ client/components/CalendarPage.jsx
@@ -33,7 +33,7 @@ class CalendarPage extends React.Component {
       <div className='container'>
         <Jumbotron>
           <p>Choose type of calendar to import.</p>
-        <Button bsStyle='primary' className='Googlebtn' onClick={() => this.GoogleFunc} > Google </Button>
+        <Button bsStyle='primary' className='Googlebtn' onClick={() => getCalendars()} > Google </Button>
         <p>     </p>
         <Button bsStyle='primary' className='Outlookbtn' onClick={() => this.calendarType = 'Outlook'}>Outlook </Button>
         </Jumbotron>
