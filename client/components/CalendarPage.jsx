fa0ae5d909ddec922942230a55d16ce1242f0547
Winnie <winnielin@pal-nat186-60-29.itap.purdue.edu>
2018-02-18 17:14:44 -0500

    add href to test button

--- client/components/CalendarPage.jsx
+++ client/components/CalendarPage.jsx
@@ -40,7 +40,7 @@ class CalendarPage extends React.Component {
          <p>     </p>
         <Button bsStyle='primary' className='alertbtn' onClick={()=>{ alert('alert'); }}>alert</Button>
         <p>     </p>
-        <Button bsStyle='primary' className='testbtn' onClick={()=>{ setActiveView(ImportPage); }}>test</Button>
+        <Button bsStyle='primary' className='testbtn' href = './ImportPage'>test</Button>
         </Jumbotron>
       </div>
   )
