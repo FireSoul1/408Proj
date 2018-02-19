1411cfb969c00fb4de51fe0cd591e9dc09c685a6
Winnie <winnielin@pal-nat186-60-29.itap.purdue.edu>
2018-02-18 19:45:50 -0500

    update on click func

--- client/components/CalendarPage.jsx
+++ client/components/CalendarPage.jsx
@@ -16,7 +16,7 @@ class CalendarPage extends React.Component {
    
       this.calendarType = ''
       this.calendarExist =  false
-      this.GoogleFunc = this.GoogleFunc.bind(this);
+     // this.GoogleFunc = this.GoogleFunc.bind(this);
   }
 
   GoogleFunc(){
