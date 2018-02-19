f768533e932fd4e4b3662f3172f5334a587d2002
Deepthi Naidu <naidud@pal-nat186-123-182.itap.purdue.edu>
2018-02-18 21:32:16 -0500

    new change

--- client/components/CalendarPage.jsx
+++ client/components/CalendarPage.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
+import {
+  Button,
+  FormControl,
+  FormGroup,
+  Jumbotron
+} from 'react-bootstrap'
+import { map } from 'lodash'
+import SweetAlert from 'react-bootstrap-sweetalert'
+import ImportPage from './ImportPage'
+
 class CalendarPage extends React.Component {
-	 render() {
-	 }
-	}
-	export default CalendarPage
+  
+  constructor(props) {
+    super(props)
+   
+      this.calendarType = ''
+      this.calendarExist =  false
+     // this.GoogleFunc = this.GoogleFunc.bind(this);
+  }
+
+  GoogleFunc(){
+//    const { advice, authorized, getCalendars, getCalendarType, getLogout, setActiveView } = this.props
+
+     this.calendarType = 'Google'
+      //setActiveView(ImportPage)
+    //this.getCalendars()
+     alert('alert');
+
+
+  }
+
+  
+  // buttons for importing google and outlook calendars
+
+   render() {
+    return (
+      <div className='container'>
+        <Jumbotron>
+          <p>Choose type of calendar to import.</p>
+        <Button bsStyle='primary' className='Googlebtn' onClick={() => this.props.setActiveView(ImportPage)}> Google </Button>
+        <p>     </p>
+        <Button bsStyle='primary' className='Outlookbtn' >Outlook </Button>
+         <p>     </p>
+        <Button bsStyle='primary' className='alertbtn' onClick={()=>{ alert('alert'); }}>alert</Button>
+         <p>     </p>
+        <Button bsStyle='primary' className='testbtn' href = 'ImportPage'>test</Button>
+         <p>     </p>
+        <Button bsStyle='primary' className='testbtn' onClick = {this.GoogleFunc()} >test2</Button>
+        </Jumbotron>
+      </div>
+  )
+   }
+  }
+  export default CalendarPage
 	
 
 	
